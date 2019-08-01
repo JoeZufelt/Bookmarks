9f2ec81c02d9b35ad9e47d9b046b97025e1c1d76
Joe Zufelt <joe.zufelt@gmail.com>
2019-07-31 22:23:15 -0400

    hide and seek 3: the third time

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,10 +1,12 @@
+var nyt_meatball = config.nyt_meatball;
+var meetup_meatball = config.meetup_meatball;
+var google_meatball = config.google_meatball;
+
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.carousel').carousel();
 
-    var nyt_meatball = config.nyt_meatball;
-    // var meetup_meatball = config.meetup_meatball;
-    // var google_meatball = config.google_meatball;
+    // var nyt_meatball = config.nyt_meatball;
 
 //API call using fetch -- above is the prerequisite code for utilizing the fetch method
 fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=' + nyt_meatball, {
@@ -60,7 +62,7 @@ fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json
         event.preventDefault();
         var zip = $("#zip").val().trim();
         var radius = $("#radius").val().trim();
-        var meetup_meatball = config.meetup_meatball;
+        // var meetup_meatball = config.meetup_meatball;
         var meetupURL = "https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/groups?&key=" + meetup_meatball + "&sign=true&photo-host=public&zip=" + zip + "&country=us&location=orlando&radius=" + radius + "&category=18&page=20";
         console.log(meetupURL);
         $.ajax({
@@ -99,8 +101,8 @@ var jsoncall = {
             "https://book-recommender-228801.firebaseapp.com/__/auth/handler"
         ],
         "javascript_origins": [
-            "http://localhost",
-            "http://localhost:5000",
+            "https://localhost",
+            "https://localhost:5000",
             "https://book-recommender-228801.firebaseapp.com"
         ]
     }
@@ -110,7 +112,7 @@ var jsoncall = {
 $("#search-btn").on("click", function displayCategory() {
     event.preventDefault();
     var category = $("#search").val().trim();
-    var google_meatball = config.google_meatball;
+    // var google_meatball = config.google_meatball;
     var categoryURL = "https://www.googleapis.com/books/v1/volumes?q=genre=" + category + "&key=" + google_meatball
     console.log(categoryURL);
 
